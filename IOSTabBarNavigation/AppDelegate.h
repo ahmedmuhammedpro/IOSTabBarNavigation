8f507bbfb7bff7791e88a22b2be8d061f70e3343
ahmedpro <ahmedmuhammedpro@gmail.com>
2020-03-18 03:31:36 +0200

    Initial Commit

---
+++ IOSTabBarNavigation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IOSTabBarNavigation
+//
+//  Created by ahmedpro on 3/18/20.
+//  Copyright © 2020 ahmedpro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
